ef4c273a032ae8b6559d1d517e0f59699ef8ee0b
AngelaHdez <angelahdezd@gmail.com>
2012-10-22 16:57:15 +0200

    Update test/tc_rps.rb

--- test/tc_rps.rb
+++ test/tc_rps.rb
@@ -1,9 +1,9 @@
-
+	
 require "/home/alu4079/Desktop/LPP repositorio/practica 5/lib/rps"
 require "test/unit"
 
 
-class TestRockPapperScissors < Test::Unit::TestCase
+class TestRockPaperScissors < Test::Unit::TestCase
 
         def setup
 		#def test variable
@@ -12,28 +12,34 @@ class TestRockPapperScissors < Test::Unit::TestCase
 	def teardown
 		#nothing to do
 	end
+	
+	#********** Ver si las respuestas no se repiten *************************
+ 	def correct_answer(x)
+ 	  return true if x =~ /Empate: /
+ 	  
+    return true if x =~ /Gana maquina. Maquina  (\w+) versus Jugador (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
+    return true if x =~ /Yupi gana el jugador. Jugador: (\W+) versus Maquina: (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
+ 	end
+ 	
 #***********************************test de juego**************************
 	def test_play_1
 	  30.times do
-	    assert correct_answer(RockPapperScissors.play('tijeras'))
+	    assert correct_answer(RockPaperScissors.play('tijeras'))
 		end	
 	
 		30.times do
-		  assert correct_answer(RockPapperScissors.play('papel'))
+		  assert correct_answer(RockPaperScissors.play('papel'))
   	end #acaba test_play
 
     30.times do
-  		assert correct_answer(RockPapperScissors.play('piedra'))
+  		assert correct_answer(RockPaperScissors.play('piedra'))
   	end  	
 	end #acaba test_play
 
 #******************** Wrong play ****************************************
  	def test_wrong_play
-        	assert_raise (SyntaxError) {RockPapperScissors.play("huracan")}
+        	assert_raise (SyntaxError) {RockPaperScissors.play('huracan')}
  	end
 
-#********** Ver si las respuestas no se repiten *************************
- 	def correct_answer(x)
- 	return true if x =- /Empate: /
- 	end
+
 end	
